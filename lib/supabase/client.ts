fd1e0a9f42892a1d3493955723e02f133c700922
Jazmin Baez <baezdjazmin@gmail.com>
2025-08-06 12:18:47 -0300

    chore: add env

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://fkcbldetobxaqayutais.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZrY2JsZGV0b2J4YXFheXV0YWlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxNzg0MTIsImV4cCI6MjA2OTc1NDQxMn0.5hn0SdK4eAdYvgFoWN4421_4U5CK1jb86XZlf4ppJ48"
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase environment variables');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
